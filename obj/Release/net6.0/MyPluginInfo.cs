e0a66d82b3351e1b94c841b34e13715cf6ed7826
Moobsmc <moobsmc@gmail.com>
2025-03-01 10:18:18 +0100

    Gitignore changes.

--- obj/Release/net6.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace PriorityQueue
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "PriorityQueue";
-        public const string PLUGIN_NAME = "PriorityQueue";
-        public const string PLUGIN_VERSION = "0.1.10";
-    }
-}

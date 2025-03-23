540da1b0c223ec231d069bcbe30886c3d5e39c7d
helskog <simonhelskog@outlook.com>
2025-03-23 19:46:24 +0100

    Added hot-reloading w/ bloodstone per request from a server owner

--- Plugin.cs
+++ Plugin.cs
@@ -1,36 +1,40 @@
 ﻿using BepInEx;
 using BepInEx.Unity.IL2CPP;
+
 using HarmonyLib;
+
 using VampireCommandFramework;
 
 namespace PriorityQueue;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 [BepInDependency("gg.deca.VampireCommandFramework")]
+[BepInDependency("gg.deca.Bloodstone")]
+[Bloodstone.API.Reloadable]
 public class Plugin : BasePlugin
 {
-    Harmony _harmony;
-    private Data _data;
+	private Harmony _harmony;
+	private Data _data;
 
-    public override void Load()
-    {
-        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} version {MyPluginInfo.PLUGIN_VERSION} is loaded!");
+	public override void Load()
+	{
+		Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} version {MyPluginInfo.PLUGIN_VERSION} is loaded!");
 
-        // Load config data
-        Data.Instance.LoadConfig();
+		// Load config data
+		Data.Instance.LoadConfig();
 
-        // Harmony patching
-        _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
-        _harmony.PatchAll(System.Reflection.Assembly.GetExecutingAssembly());
+		// Harmony patching
+		_harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+		_harmony.PatchAll(System.Reflection.Assembly.GetExecutingAssembly());
 
-        // Register all commands in the assembly with VCF
-        CommandRegistry.RegisterAll();
-    }
+		// Register all commands in the assembly with VCF
+		CommandRegistry.RegisterAll();
+	}
 
-    public override bool Unload()
-    {
-        CommandRegistry.UnregisterAssembly();
-        _harmony?.UnpatchSelf();
-        return true;
-    }
+	public override bool Unload()
+	{
+		CommandRegistry.UnregisterAssembly();
+		_harmony?.UnpatchSelf();
+		return true;
+	}
 }
